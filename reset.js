bf6cb8d5e17391818a0eb10021906ad2732e7a4a
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2024-11-07 16:13:40 +0000

    fixed temp text

--- reset.js
+++ reset.js
@@ -17,4 +17,5 @@ function resetcfm(){
     document.getElementById("rstcfm").style.display="none";
     document.getElementById("score").innerHTML = 0
     document.getElementById("golden").style.display="none";
+    document.getElementById("prz").innerHTML = "Please take a ball and roll it on the track!"
 }
