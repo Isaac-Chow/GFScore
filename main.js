2a50a5c4176223fb3bc6956b19295aeb79321d36
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2024-11-09 04:32:28 +0000

    prize()

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ window.addEventListener('keydown', function (e) {
     if(e.key == "x"){
         console.log("1")
         document.getElementById("score").innerHTML = current +1
+        prize()
     }
     if(e.key == "z"){
         console.log("3")
         document.getElementById("score").innerHTML = current +3
+        prize()
     }
     if(e.key == "j"){
         console.log("5")
         document.getElementById("score").innerHTML = current +5
+        prize()
     }
     if(e.key == "h"){
         console.log("10")
         document.getElementById("score").innerHTML = current +10
+        prize()
     }
-    prize()
   }, false);
